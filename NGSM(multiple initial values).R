7e1d4610e9910aac8accada337e2e1131b4e2f3a
Sangkon Oh <49501844+ohsangkon@users.noreply.github.com>
2024-02-27 21:20:23 +0900

    Update NGSM(multiple initial values).R

--- NGSM(multiple initial values).R
+++ NGSM(multiple initial values).R
@@ -2,7 +2,7 @@
 ### NGSM
 ###########################################################
 
-fmix_reg_scalemix_iter =function(formula, data , m = 2, iter = 5, p=NULL,beta=NULL,con=NULL,ini_sigma=NULL,maxitr=100, prob = 1, tRatio = 0.05){
+fmix_reg_scalemix_iter =function(formula, data , m = 2, iter = 5, p=NULL,beta=NULL,con=NULL,ini_sigma=NULL,maxitr=100, tRatio = 0.05){
 	### Find maximum likelihood reg_scalemix  ###
 	model = list()
 	likelihood <- c()
@@ -10,9 +10,9 @@ fmix_reg_scalemix_iter =function(formula, data , m = 2, iter = 5, p=NULL,beta=NU
 	if(m != 1){
 		for(idx in 1 : iter){
 	tryCatch(
-			model[[idx]] <- fmix_reg_scalemix(formula, data,m, p, beta, con,ini_sigma,maxitr=100, prob, tRatio = tRatio),
+			model[[idx]] <- fmix_reg_scalemix(formula, data,m, p, beta, con,ini_sigma,maxitr=100, tRatio = tRatio),
 		error = function(e){
-			model[[idx]] <- fmix_reg_scalemix(formula, data,m, p, beta, con,ini_sigma,maxitr=100, prob, tRatio = tRatio)
+			model[[idx]] <- fmix_reg_scalemix(formula, data,m, p, beta, con,ini_sigma,maxitr=100, tRatio = tRatio)
 		}
 
 	)
@@ -21,7 +21,7 @@ fmix_reg_scalemix_iter =function(formula, data , m = 2, iter = 5, p=NULL,beta=NU
 		}
 		return(model[[which.max(likelihood)]])
 	}else{
-  		model = fmix_reg_scalemix(formula, data,m, p, beta, con,ini_sigma,maxitr=100, prob, tRatio = tRatio)
+  		model = fmix_reg_scalemix(formula, data,m, p, beta, con,ini_sigma,maxitr=100, tRatio = tRatio)
 		return(model)
 	}
 
